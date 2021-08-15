a3ebafc29abf55268263761372cbeffe51f086cc
Richard Lau <rlau@redhat.com>
2021-08-15 20:38:07 +0100

    Allow setting user and group ID (#41)
    Allow the user ID and group IDs to be changed when preparing the
    images for running/testing on systems where the host is running
    under a user with a user ID other than 1000.

--- bin/prepare-images.sh
+++ bin/prepare-images.sh
@@ -6,7 +6,7 @@ __dirname="$(CDPATH= cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 image_tag_pfx=unofficial-build-recipe-
 
 for recipe in $(ls ${__dirname}/../recipes/); do
-	docker build ${__dirname}/../recipes/${recipe}/ -t ${image_tag_pfx}${recipe}
+	docker build ${__dirname}/../recipes/${recipe}/ -t ${image_tag_pfx}${recipe} --build-arg UID=1000 --build-arg GID=1000
 done
 
 docker system prune -f
