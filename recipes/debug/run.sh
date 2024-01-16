ff5593601ca4775d85f6abe02eec00e1cb7979ee
matthewkeil <me@matthewkeil.com>
2024-01-16 03:40:22 -0500

    feat: add debug recipe

---
+++ recipes/debug/run.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+release_urlbase="$1"
+disttype="$2"
+customtag="$3"
+datestring="$4"
+commit="$5"
+fullversion="$6"
+source_url="$7"
+config_flags=""
+
+cd /home/node
+
+tar -xf node.tar.xz
+cd "node-${fullversion}"
+
+export CC="ccache gcc"
+export CXX="ccache g++"
+
+./configure --debug
+
+make -j$(getconf _NPROCESSORS_ONLN) binary V= \
+  DESTCPU="x64" \
+  ARCH="x64" \
+  VARIATION="" \
+  DISTTYPE="$disttype" \
+  CUSTOMTAG="$customtag" \
+  DATESTRING="$datestring" \
+  COMMIT="$commit" \
+  RELEASE_URLBASE="$release_urlbase" \
+  CONFIG_FLAGS="$config_flags"
+
+mv node-*.tar.?z /out/
