d1525f8a6862392ef02a02268d5c7ef81d91097c
matthewkeil <me@matthewkeil.com>
2024-01-16 23:00:02 -0500

    fix: update x64-debug run.sh

--- recipes/x64-debug/run.sh
+++ recipes/x64-debug/run.sh
@@ -23,7 +23,7 @@ export CXX="ccache g++"
 make -j$(getconf _NPROCESSORS_ONLN) binary V= \
   DESTCPU="x64" \
   ARCH="x64" \
-  VARIATION="" \
+  VARIATION="debug" \
   DISTTYPE="$disttype" \
   CUSTOMTAG="$customtag" \
   DATESTRING="$datestring" \
@@ -31,4 +31,25 @@ make -j$(getconf _NPROCESSORS_ONLN) binary V= \
   RELEASE_URLBASE="$release_urlbase" \
   CONFIG_FLAGS="$config_flags"
 
-mv node-*.tar.?z /out/
+# The default tarballs from the binary make target do not have the debug
+# binary included so we need to rebuild them correctly.
+for tarball in node-*.tar.?z; do
+  # Create a temporary directory and extract the tarball into it
+  temp_dir=$(mktemp -d)
+  tar -xf "$tarball" -C "$temp_dir"
+
+  # Copy the debug build to a different name
+  cp out/Debug/node "$temp_dir/bin/node_g"
+
+  # Recreate the tarball with the same comprbasession format
+  case $tarball in
+    *.tar.gz)
+      tar -czf "/out/$tarball" -C "$temp_dir" .
+      ;;
+    *.tar.xz)
+      tar -cJf "/out/$tarball" -C "$temp_dir" .
+      ;;
+  esac
+
+  rm -rf "$temp_dir"
+done
