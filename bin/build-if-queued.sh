b86431f3b910eccdef3563600aa9ee3e4e8c4ac8
matthewkeil <me@matthewkeil.com>
2024-02-03 17:39:25 +0700

    feat: add handling of recipes to build-if-queued.sh

--- bin/build-if-queued.sh
+++ bin/build-if-queued.sh
@@ -22,10 +22,26 @@ exit_if_locked "node-build"
 acquire_lock "node-build"
 
 # next build from the queue
-fullversion="$(${__dirname}/queue-pop.sh)"
+version_and_recipes="$(${__dirname}/queue-pop.sh)"
 
-if [ "X$fullversion" != "X" ]; then
+if [ "X$version_and_recipes" != "X" ]; then
+  # split the version and recipes
+  IFS='|' read -r fullversion recipes_list <<< "$version_and_recipes"
+  
   decode "$fullversion"
+
+  cmd_args=("-v" "$fullversion")
+
+  # Check if recipes_list is not empty
+  if [[ -n "$recipes_list" ]]; then
+    # split comma separated list of recipes_to_build
+    IFS=',' read -ra recipes_to_build <<< "$recipes_list"
+    for recipe in "${recipes_to_build[@]}"; do
+      # Construct the recipes_args as part of the command string
+      cmd_args+=("-r" "$recipe")
+    done
+  fi
+
   # only "release" versions are guaranteed to have source files available when they appear
   # in the indexes, the others promote components as they are available and source has a
   # lower priority
@@ -33,10 +49,10 @@ if [ "X$fullversion" != "X" ]; then
   # that a version gets stuck in the queue and this `curl` to be run over and over
   # but since it's a FIFO it shouldn't prevent others from running
   if curl --output /dev/null --silent --head --fail $source_url; then
-    ${__dirname}/build.sh $fullversion
+    "${__dirname}/build.sh" "${cmd_args[@]}"
   else
     echo "Source file for ${fullversion} does not exist, queueing for retry"
-    ${__dirname}/queue-push.sh $fullversion
+    "${__dirname}/queue-push.sh" "${cmd_args[@]}"
   fi
 fi
 
