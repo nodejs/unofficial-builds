fd6e06bd7512402293b3f3ac2fd65a6130404c9f
matthewkeil <me@matthewkeil.com>
2024-02-03 23:08:02 +0700

    fix: prepare fetch-source

--- bin/prepare-images.sh
+++ bin/prepare-images.sh
@@ -7,8 +7,11 @@ __dirname="$(CDPATH= cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "${__dirname}/_config.sh"
 source "${__dirname}/_get_recipes.sh"
 
+docker build "${__dirname}/../fetch-source" -t "${image_tag_pfx}fetch-source" --build-arg UID=1000 --build-arg GID=1000
+
 for recipe in $all_recipes; do
-	docker build ${__dirname}/../recipes/${recipe}/ -t ${image_tag_pfx}${recipe} --build-arg UID=1000 --build-arg GID=1000
+	docker build "${__dirname}/../recipes/${recipe}/" -t "${image_tag_pfx}${recipe}" --build-arg UID=1000 --build-arg GID=1000
 done
+
 
 docker system prune -f
