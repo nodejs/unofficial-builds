bddb722eabdbd326e8503883bbfd6416a06e1bec
Robin Wong <chaowang0313@gmail.com>
2024-08-24 04:20:34 +0000

    fix: https://github.com/c-ares/c-ares/issues/850

--- recipes/x64-glibc-217/run.sh
+++ recipes/x64-glibc-217/run.sh
@@ -17,9 +17,14 @@ cd /home/node
 tar -xf node.tar.xz
 
 # configuring cares correctly to not use sys/random.h on this target
-cd "node-${fullversion}"/deps/cares/config/linux
-sed -i 's/define HAVE_SYS_RANDOM_H 1/undef HAVE_SYS_RANDOM_H/g' ./ares_config.h
-sed -i 's/define HAVE_GETRANDOM 1/undef HAVE_GETRANDOM/g' ./ares_config.h
+cd "node-${fullversion}"/deps/cares
+sed -i 's/define HAVE_SYS_RANDOM_H 1/undef HAVE_SYS_RANDOM_H/g' ./config/linux/ares_config.h
+sed -i 's/define HAVE_GETRANDOM 1/undef HAVE_GETRANDOM/g' ./config/linux/ares_config.h
+
+# fix https://github.com/c-ares/c-ares/issues/850
+if [[ "$(grep -o 'ARES_VERSION_STR "[^"]*"' ./include/ares_version.h | awk '{print $2}' | tr -d '"')" == "1.33.0" ]]; then
+  sed -i 's/MSG_FASTOPEN/TCP_FASTOPEN_CONNECT/g' ./src/lib/ares__socket.c
+fi
 
 cd /home/node
 
@@ -29,7 +34,8 @@ export CC="ccache gcc"
 export CXX="ccache g++"
 export MAJOR_VERSION=$(echo ${fullversion} | cut -d . -f 1 | tr --delete v)
 
-. /opt/rh/devtoolset-9/enable
+. /opt/rh/devtoolset-11/enable
+. /opt/rh/rh-python38/enable
 
 make -j$(getconf _NPROCESSORS_ONLN) binary V= \
   DESTCPU="x64" \
