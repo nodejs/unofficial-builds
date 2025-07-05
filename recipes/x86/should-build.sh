3f940e52b1c2a5ac9329847b4d8be030220db641
vitaliylag <vitaliylagunov@gmail.com>
2025-07-05 09:17:10 +0400

    Update should-build.sh and add comments

--- recipes/x86/should-build.sh
+++ recipes/x86/should-build.sh
@@ -8,4 +8,4 @@ fullversion=$2
 decode "$fullversion"
 
 [ "$major" -ge 7 ] || ( [ "$major" -eq 6 ] && [ "$minor" -ge 2 ] )  # Node.js v6.1- cannot download required files due to broken links
-[[ ! "$fullversion" =~ ^v22\.[0-2]\. ]]                             # Not supported neigher by GCC 15.1 nor GCC 9.3 (GCC 12.4 would be good but is not installed)
+[[ ! "$fullversion" =~ ^v22\.[0-2]\. ]]                             # GCC version between 9.3 and 15.1 is required but not installed
