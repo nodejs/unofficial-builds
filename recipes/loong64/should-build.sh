f973431d93fcdb7a74bf6088d242ccf09835461b
Xiao-Tao <zhoumingtao@loongson.cn>
2025-09-02 11:46:47 +0800

    fix(loong64): address "The GID `1000' is already in use" error. (#186)
    If the group/user with the specified GID/UID already exists,
    it will be changed to node:node.
    similar to recipes/riscv64/Dockerfile.
    
    Co-authored-by: Mingtao Zhou <mingtaoxt@gmail.com>

--- recipes/loong64/should-build.sh
+++ recipes/loong64/should-build.sh
@@ -7,8 +7,7 @@ fullversion=$2
 
 decode "$fullversion"
 
-(test "$major" -eq "18" && test "$minor" -ge "18") || \
 (test "$major" -eq "20" && test "$minor" -ge "10") || \
 (test "$major" -eq "21") || \
 (test "$major" -eq "22" && test "$minor" -ge "14") || \
-(test "$major" -eq "23")
+(test "$major" -ge "23")
