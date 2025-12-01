8269faa5e26b6d7ab3a57d2052bd357bfc385b87
mattias.michaux <mattias.michaux@gmail.com>
2025-12-01 08:29:52 +0100

    Bump dist-indexer version to support the last new unofficial builds

--- bin/_config.sh
+++ bin/_config.sh
@@ -17,7 +17,7 @@ recipes=(
 # This should be updated as new versions of nodejs-dist-indexer are released to
 # include new assets published here; this is not done automatically for security
 # reasons.
-dist_indexer_version=v1.7.1
+dist_indexer_version=v1.7.26
 
 image_tag_pfx=unofficial-build-recipe-
 
