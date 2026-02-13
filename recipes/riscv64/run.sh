a5d5fd03cb15637f8b651f35310d61f6f75b773e
Stewart X Addison <sxa@ibm.com>
2026-02-13 16:50:04 +0000

    fix: make ccache work properly by using CCACHE_BASEDIR
    Signed-off-by: Stewart X Addison <sxa@ibm.com>

--- recipes/riscv64/run.sh
+++ recipes/riscv64/run.sh
@@ -18,6 +18,7 @@ cd /home/node
 tar -xf node.tar.xz
 cd "node-${fullversion}"
 
+export CCACHE_BASEDIR="$PWD"
 export CC_host="ccache gcc-13"
 export CXX_host="ccache g++-13"
 export CC="ccache /usr/bin/riscv64-linux-gnu-gcc-14"
